c62d008c24a5b77c0999aab7c9fec5f2de00f02e
KenzoFukushima <20241ctb0100081@estudantes.ifpr.edu.br>
2026-08-22 14:30:14 -0300

    corrigindo Login submit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,39 +14,40 @@ class AuthController extends Controller
     public function loginSubmit(Request $request) {
         $request->validate(
             [
-                'emailUser' => 'required|min:8',
+                'emailUser' => 'required|email|min:8',
                 'passwordUser' => 'required|min:6',
             ],
             [
-                'emailUser.required' => 'O campo email é obragatório',
+                'emailUser.required' => 'O campo email é obrigatório.',
                 'emailUser.email' => 'Email inválido.',
-                'emailUser.min' => 'Precisa ter no mínimo 8 caracteres',
+                'emailUser.min' => 'Precisa ter no mínimo 8 caracteres.',
 
                 'passwordUser.required' => 'O campo senha é obrigatório.',
-                'passwordUser.min' => 'Precisa ter no mínimo 6 caracteres',
-
+                'passwordUser.min' => 'Precisa ter no mínimo 6 caracteres.',
             ]
         );
 
         $emailUser = $request->input('emailUser');
         $password = $request->input('passwordUser');
         
+
         $user = User::where('email', $emailUser)->whereNull('deleted_at')->first();
 
         if(!$user){
             return redirect()->back()
                     ->withInput()
-                    ->with('login_error','Username ou password incorretos!');
-        } else {
-            if(!password_verify($password,$user->password)){
-                return redirect()->back()
-                        ->withInput()
-                        ->with('login_error','Username ou password incorretos!');
-            }
+                    ->with('login_error','E-mail ou password incorretos!');
+        }
+
+        if(!password_verify($password, $user->password)){
+            return redirect()->back()
+                    ->withInput()
+                    ->with('login_error','E-mail ou password incorretos!');
         }
         
         $user->last_login = date('Y-m-d H:i:s');
-        $user->save(); //Persistir no BD
+        $user->save(); 
+
         session([
             'user' => [
                 'id' => $user->id,
@@ -54,7 +55,7 @@ class AuthController extends Controller
             ]
         ]);
 
-
-       return redirect('/');
+        return redirect('/');
     }
+
 }
